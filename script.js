17da041d3563039043697d94688ba0b6075845c0
Snehal Gupta <133367387+snehalgupta13@users.noreply.github.com>
2023-06-24 07:02:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
+	for(let x in sampleObject){
+		if(x===key){
+			return true;
+		}
+	}
+	return false;
 }
 
 // Do not change the code below
